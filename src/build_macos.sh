ee385c87cea88f4a73a290bafd087b57f29ec152
redabs <redabs@users.noreply.github.com>
2025-08-12 04:43:52 +0900

    fix macos build

--- src/build_macos.sh
+++ src/build_macos.sh
@@ -1,14 +1,15 @@
 set -o xtrace
 
-
 SCRIPT_DIR=$(dirname "$(realpath $0)")
 SOURCE_PATH=$SCRIPT_DIR/
 
 mkdir -p build
 cd build
 
-CFLAGS="-O0 -Wall -g -fsanitize=address -g"
+CFLAGS="-O0 -Wall -g -fsanitize=address -g -Wno-missing-braces"
 HEADERS="-F/Library/Frameworks"
-LIBS="-rpath /Library/Frameworks -framework -SDL3 -lm"
+LIBS="-rpath /Library/Frameworks -framework SDL3 -lm"
 
-gcc $SOURCE_PATH/k_sdl.cpp $SOURCE_PATH/kalam.cpp $SOURCE_PATH/stb_truetype.cpp $CFLAGS $LIBS -o kalam
+gcc $SOURCE_PATH/k_sdl.cpp $SOURCE_PATH/kalam.cpp $SOURCE_PATH/stb_truetype.cpp $CFLAGS $HEADERS $LIBS -o kalam_macos
+
+cd -
